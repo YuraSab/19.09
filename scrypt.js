89cb01024b642f15c58f164a47b327186920bd6b
YuraSab <yrasabat@gmail.com>
2024-09-19 18:40:51 +0300

    slider

--- scrypt.js
+++ scrypt.js
@@ -24,3 +24,53 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+//  switcher of mute
+document.addEventListener('DOMContentLoaded', function () {
+    const muteIcon = document.getElementById('icon_mute');
+    const unmuteIcon = document.getElementById('icon_unmute');
+
+    muteIcon.addEventListener('click', function () {
+        muteIcon.style.display = 'none';
+        unmuteIcon.style.display = 'block';
+    });
+
+    unmuteIcon.addEventListener('click', function () {
+        unmuteIcon.style.display = 'none';
+        muteIcon.style.display = 'block';
+    });
+});
+
+
+
+
+// slider/swiper
+const slides = [
+    { id: 1, imgPath: "assets/pictures/girl_one.jpg", title: "Step-Sister" },
+    { id: 2, imgPath: "assets/pictures/girl_two.jpg", title: "Your Neighbor" },
+    { id: 3, imgPath: "assets/pictures/girl_three.jpg", title: "Famous Celebrity" }
+];
+
+let currentSlideIndex = 0;
+
+function showSlide(index) {
+    const slide = slides[index];
+    document.getElementById('slider_image').src = slide.imgPath;
+    document.getElementById('slider_title').textContent = slide.title;
+}
+
+function prevSlide() {
+    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+    showSlide(currentSlideIndex);
+}
+function nextSlide() {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    showSlide(currentSlideIndex);
+}
+
+showSlide(currentSlideIndex);
+
+
+
+
